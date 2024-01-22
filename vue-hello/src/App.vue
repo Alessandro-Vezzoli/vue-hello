4fc567cf6d7281a9f8643baf1e15f3840dd06d7b
alessandro vezzoli <alessandro.vezzoli@vp360.eu>
2024-01-22 11:28:40 +0100

    created vue app and message with data

---
+++ vue-hello/src/App.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      message: "MESSAGGIO STAMPATO ATTRAVERSO DATA ",
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    {{ message }}
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
